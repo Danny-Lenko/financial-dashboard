1102eb84b24f9b3f39f49fdaa4eb1a1bb69b912a
Danny-Lenko <danylenko.14@gmail.com>
2025-11-09 19:06:13 +0200

    configured prettier & absolute paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-// TODO:
-// 1. Add linter and prettier configurations
-// 2. Set up absolute imports
-// 3. Configure environment variables
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
